2bbcd23c64b2d32b996bb1b2577261e97d53a51e
Kevin Oliveira <me@kevinoliveira.com.br>
2016-12-30 18:08:03 -0200

    Create task to clean app folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var clean = require('gulp-clean');
 var pug = require('gulp-pug');
+var gulpCopy = require('gulp-copy');
+
+gulp.task('clean', function cleanAppFolder () {
+  return gulp.src('app', {read: false})
+    .pipe(clean());
+});
+
+var srcFiles = [ 'src/assets/*', '!src/assets/sass/*' ];
+var srcDest = 'app/assets';
+
+gulp.task('copy', function copyFiles() {
+  return
+    gulp.src(srcFiles)
+    .pipe(gulpCopy())
+    .pipe(gulp.dest(srcDest));
+});
 
 gulp.task('views', function buildHTML() {
   return gulp.src('src/*.pug')
@@ -8,4 +25,4 @@ gulp.task('views', function buildHTML() {
   .pipe(gulp.dest('app'));
 });
 
-gulp.task('default', ['views']);
+gulp.task('default', ['clean', 'views', 'copy']);
